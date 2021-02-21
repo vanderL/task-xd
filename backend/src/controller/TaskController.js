30b85b4dfbf28eea32f97bccfd43e37c4f82ae76
vanderL <vnd.vander@gmail.com>
2021-02-21 20:36:12 -0300

    Showing and Deleting Tasks

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -36,6 +36,28 @@ class TaskController {
             })
     }
 
+    async show(req, res) {
+        await TaskModel.findById(req.params.id)
+        .then(response => {
+            if(response)
+                return res.status(200).json(response)
+            else
+                return res.status(404).json({error: 'Task not found!'})
+        }).catch(err => {
+            return res.status(500).json(err)
+        })
+    }
+
+    async delete(req, res) {
+        await TaskModel.deleteOne({'_id': req.params.id})
+            .then(response => {
+                return res.status(200).json(response)
+            })
+            .catch(err => {
+                return res.status(500).json(err)
+            })
+    }
+
 }
 
 module.exports = new TaskController()
--- backend/src/router/TaskRouter.js
+++ backend/src/router/TaskRouter.js
@@ -6,7 +6,9 @@ const TaskValidation = require('../middlewares/TaskValidation')
 const MacaddressValidation = require('../middlewares/MacaddressValidation')
 
 router.post('/',TaskValidation ,TaskController.create)
+router.get('/:id', TaskController.show)
 router.put('/:id',TaskValidation, TaskController.update)
+router.delete('/:id', TaskController.delete)
 router.get('/filter/all',MacaddressValidation, TaskController.all)
 
 module.exports = router
