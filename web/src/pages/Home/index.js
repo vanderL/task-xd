8aa88de7f4886cf8407f999e5dcbe8203e05ead6
vanderL <vnd.vander@gmail.com>
2021-03-10 21:35:29 -0300

    Init Frontend

---
+++ web/src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Home() {
+    return (
+      <h1>Home né pai!!</h1>
+    );
+  }
+  
+  export default Home;
+  
---
+++ web/src/pages/QrCode/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function QrCode() {
+    return (
+      <h1>QrCode!!</h1>
+    );
+  }
+  
+  export default QrCode;
+  
