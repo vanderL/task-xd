6d768825307a217e82bdf5427553786387f23bc4
vanderL <vnd.vander@gmail.com>
2021-03-22 13:02:18 -0300

    Home: Filter

--- mobileNew/src/pages/Home/index.js
+++ mobileNew/src/pages/Home/index.js
@@ -1,18 +1,43 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
 
 import styles from './styles'
 
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 
-export default function Home() {    
+export default function Home() {  
+    const [filter, setFilter] = useState('today')
+    
     return(
         <View style={styles.container}>
             <Header 
                 showNotification={true}
                 showBack={false} 
             />
+
+            <View style={styles.filter}>
+                <TouchableOpacity onPress={() => setFilter('all')}>
+                    <Text style={filter == 'all' ? styles.filterTextActived : styles.filterTextInatived}>Todos</Text>        
+                </TouchableOpacity>
+                
+                <TouchableOpacity onPress={() => setFilter('today')}>
+                    <Text style={filter == 'today' ? styles.filterTextActived : styles.filterTextInatived}>Hoje</Text>        
+                </TouchableOpacity>
+                
+                <TouchableOpacity onPress={() => setFilter('week')}>
+                    <Text style={filter == 'week' ? styles.filterTextActived : styles.filterTextInatived}>Semana</Text>        
+                </TouchableOpacity>
+                
+                <TouchableOpacity onPress={() => setFilter('month')}>
+                    <Text style={filter == 'month' ? styles.filterTextActived : styles.filterTextInatived}>Mês</Text>        
+                </TouchableOpacity>
+                
+                <TouchableOpacity onPress={() => setFilter('year')}>
+                    <Text style={filter == 'year' ? styles.filterTextActived : styles.filterTextInatived}>Ano</Text>        
+                </TouchableOpacity>
+            </View>
+            
             <Footer  icon={'add'}/>
 
         </View>
--- mobileNew/src/pages/Home/styles.js
+++ mobileNew/src/pages/Home/styles.js
@@ -6,6 +6,24 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'flex-start'
+    },
+    filter: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-around',
+        height: 70,
+        alignItems: 'center',
+    },
+    filterTextActived: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#ee6b26'
+    },
+    filterTextInatived: {
+        color: '#20295f',
+        fontWeight: 'bold',
+        fontSize: 16,
+        opacity: 0.5
     }
 })
 
